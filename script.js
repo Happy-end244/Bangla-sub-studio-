0894b51544649d105345974c60da12b1f3486a03
Happy-end244 <mahdivai244@gmail.com>
2025-05-31 10:13:43 +0600

    Create script.js

---
+++ script.js
@@ -0,0 +1,14 @@
+// জিমেইল লগইন ফাংশন (প্লেসহোল্ডার)
+function login() {
+    alert("Google Login will be implemented later!");
+}
+
+// প্রিমিয়াম পেজ ওপেন করবে
+function openPremiumPage() {
+    window.open("premium.html", "_blank"); // পরে এই ফাইল বানাবেন
+}
+
+// টেলিগ্রাম লিঙ্কে রিডাইরেক্ট
+function goToTelegram(username) {
+    window.open(`https://t.me/${username}`, "_blank");
+}
